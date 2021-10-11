c69b4c43edecc8680a9f87e51211cb7feeb8f1b0
zhangting <winnercrazy@126.com>
2021-10-11 16:58:57 +0800

    Learn15 New Add

--- PycharmProjects/Learn15.py
+++ PycharmProjects/Learn15.py
@@ -5,3 +5,14 @@
 # 开发工具:PyCharm
 
 #数据库操作
+import sqlite3
+
+#创建数据库，如没有自动创建
+conn = sqlite3.connect('microsoft.db')
+#游标
+cursor = conn.cursor()
+#数据库语句，创建数据
+cursor.execute('create table user (id int(10) primary key, name varchar(20))')
+cursor.close()
+conn.close()
+
