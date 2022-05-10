869f6409c317b1fc21af37dc399fe957a4b200a4
zhangting <winnercrazy@126.com>
2022-05-10 21:04:49 +0800

    new file update

---
+++ PycharmProjects/flask表单.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8
+# 开发人员:zhangting
+# 开始时间:2022/3/15 5:18 PM
+# 文件名称:flask表单
+# 开发工具:PyCharm
+
+from flask import Flask
+from wtforms import StringField,PasswordField,SubmitField
+from flask_wtf import FlaskForm
---
+++ PycharmProjects/模板.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8
+# 开发人员:zhangting
+# 开始时间:2022/3/15 5:00 PM
+# 文件名称:模板
+# 开发工具:PyCharm
+
+
+#模板 jinja2
